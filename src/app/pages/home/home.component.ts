6b6de9f42ec7aef9a1dc8a7c213c03c4de6b7d95
Nirjan Khadka <nirjan.khadka@ensue.us>
2020-02-18 12:12:14 +0545

    finish home screen design

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from "@angular/core";
 
 @Component({
     selector: "app-home",
-    templateUrl: "home.component.html"
+    templateUrl: "home.component.html",
+    styleUrls: ["./home.component.css"]
 })
 export class HomeComponent implements OnInit {
     constructor() {}
